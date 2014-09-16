9de2f9415b10ca1efb10e1eecd725ba484c7dabe
Brian Yarbrough <C16Brian.Yarbrough@usafa.edu>
2014-09-16 12:57:17 -0600

    started

--- main.asm
+++ main.asm
@@ -1,6 +1,6 @@
 ;-------------------------------------------------------------------------------
 ; MSP430 Assembler Code Template for use with TI Code Composer Studio
-;
+; Lab02
 ;
 ;-------------------------------------------------------------------------------
             .cdecls C,LIST,"msp430.h"       ; Include device header file
@@ -12,6 +12,18 @@
             .retainrefs                     ; Additionally retain any sections
                                             ; that have references to current
                                             ; section
+
+; Store Key Here
+key:	.byte	0xac
+
+;Store Message to be decoded here
+messg:	.byte	0xef, 0xc3, 0xc2, 0xcb, 0xde, 0xcd
+
+;Initialize Registers
+mTrk:		.equ		r5		;register for tracking location on encrypted message
+work:		.equ		r6		;workhrse register - this is where things will be decrypted
+dTrk:		.equ		r7		;register for tracking location on the resulting message
+
 ;-------------------------------------------------------------------------------
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
@@ -19,6 +31,9 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
                                             ; Main loop here
 ;-------------------------------------------------------------------------------
+		mov		#mssg, mTrk	;point the register at the start of the message
+		mov		#0x200, dTrk	;starting location for decrypted message
+
 
 
 ;-------------------------------------------------------------------------------
