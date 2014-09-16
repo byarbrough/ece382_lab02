85415b52b1fb3d88f47e0d65dc23a39867b5796f
Brian Yarbrough <C16Brian.Yarbrough@usafa.edu>
2014-09-16 13:45:52 -0600

    done for now

--- main.asm
+++ main.asm
@@ -16,11 +16,16 @@
 key:	.byte	0xac
 
 ;Store Message to be decoded here
-messg:	.byte	0xef, 0xc3, 0xc2, 0xcb, 0xde, 0xcd
+messg:	.byte	0xef,0xc3,0xc2,0xcb,0xde,0xcd,0xd8,0xd9,0xc0,0xcd,0xd8,0xc5,0xc3,0xc2,0xdf,0x8d,0x8c,0x8c,0xf5
+		.byte	0xc3,0xd9,0x8c,0xc8,0xc9,0xcf,0xde,0xd5,0xdc,0xd8,0xc9,0xc8,0x8c,0xd8,0xc4,0xc9,0x8c,0xe9,0xef
+		.byte	0xe9,0x9f,0x94,0x9e,0x8c,0xc4,0xc5,0xc8,0xc8,0xc9,0xc2,0x8c,0xc1,0xc9,0xdf,0xdf,0xcd,0xcb,0xc9
+		.byte	0x8c,0xcd,0xc2,0xc8,0x8c,0xcd,0xcf,0xc4,0xc5,0xc9,0xda,0xc9,0xc8,0x8c,0xde,0xc9,0xdd,0xd9,0xc5
+		.byte	0xde,0xc9,0xc8,0x8c,0xca,0xd9,0xc2,0xcf,0xd8,0xc5,0xc3,0xc2,0xcd,0xc0,0xc5,0xd8,0xd5,0x8f
+
 
 ;Initialize Registers
 mTrk:		.equ		r5		;register for tracking location on encrypted message
-work:		.equ		r6		;workhrse register - this is where things will be decrypted
+work:		.equ		r6		;workhorse register - this is where things will be decrypted
 dTrk:		.equ		r7		;register for tracking location on the resulting message
 
 ;-------------------------------------------------------------------------------
@@ -44,7 +49,7 @@ forever:    jmp     forever
 
 ;-------------------------------------------------------------------------------
 ;Subroutine Name: decryptMessage
-;Author:
+;Author: Brian Yarbrough
 ;Function: Decrypts a string of bytes and stores the result in memory.  Accepts
 ;           the address of the encrypted message, address of the key, and address
 ;           of the decrypted message (pass-by-reference).  Accepts the length of
@@ -57,15 +62,17 @@ forever:    jmp     forever
 ;-------------------------------------------------------------------------------
 
 decryptMessage:
-			mov.b	@mTrk+, work	;stage byte for decryption
+nextByte	mov.b	@mTrk+, work	;stage byte for decryption
 			call	#decryptCharacter
 			mov.b	work, 0(dTrk)
 			inc		dTrk
+			cmp.b	#0x8F, mTrk
+			jnz		nextByte
             ret
 
 ;-------------------------------------------------------------------------------
 ;Subroutine Name: decryptCharacter
-;Author:
+;Author: Brian Yarbrough
 ;Function: Decrypts a byte of data by XORing it with a key byte.  Returns the
 ;           decrypted byte in the same register the encrypted byte was passed in.
 ;           Expects both the encrypted data and key to be passed by value.
@@ -75,7 +82,11 @@ decryptMessage:
 ;-------------------------------------------------------------------------------
 
 decryptCharacter:
+			push	mTrk
+			push	dTrk
 			xor.b	key, work
+			pop		dTrk
+			pop		mTrk
             ret
 
 ;-------------------------------------------------------------------------------
